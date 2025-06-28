5c74a035881e89a11b2eda21e9de570671b9dbe9
mostlykiguess <bruvistrue93@gmail.com>
2025-06-28 12:48:08 +0530

    Add GTK4 MenuItem and style modules with examples and tests
    - Implement sugar.graphics.menuitem with MenuItem and MenuSeparator
    classes - For menuitem we have a lot of breaking changes and
    ImageMenuItem is replaced with Button. - Implement sugar.graphics.style
    with Color, Font, and style constants for GTK4 - Add example activities:
    menuitem_example.py and style_example.py - Add tests for menuitem and
    style modules

--- src/sugar/__init__.py
+++ src/sugar/__init__.py
@@ -21,11 +21,26 @@ __license__ = "LGPL-2.1-or-later"
 from .activity.activity import Activity, SimpleActivity
 from .graphics.xocolor import XoColor
 from .graphics.icon import Icon, EventIcon
+# from .graphics.tray import HTray, VTray, TrayButton, TrayIcon
+# from .graphics.window import Window, UnfullscreenButton
+from .graphics.menuitem import MenuItem, MenuSeparator
+# from .graphics.toolbox import Toolbox
+from .graphics import style
 
 __all__ = [
     "Activity",
-    "SimpleActivity", 
+    "SimpleActivity",
     "XoColor",
     "Icon",
     "EventIcon",
+    # "HTray",
+    # "VTray",
+    # "TrayButton",
+    # "TrayIcon",
+    # "Window",
+    # "UnfullscreenButton",
+    "MenuItem",
+    "MenuSeparator",
+    # "Toolbox",
+    "style",
 ]
--- src/sugar/graphics/__init__.py
+++ src/sugar/graphics/__init__.py
@@ -9,10 +9,17 @@ from .xocolor import XoColor
 from .icon import (Icon, EventIcon, CanvasIcon, CellRendererIcon,
                    get_icon_file_name, get_surface, get_icon_state,
                    SMALL_ICON_SIZE, STANDARD_ICON_SIZE, LARGE_ICON_SIZE)
+# from .tray import (HTray, VTray, TrayButton, TrayIcon,
+#                    ALIGN_TO_START, ALIGN_TO_END, GRID_CELL_SIZE)
+# from .window import Window, UnfullscreenButton
+
 
 __all__ = [
-    "XoColor", 
+    "XoColor",
     "Icon", "EventIcon", "CanvasIcon", "CellRendererIcon",
     "get_icon_file_name", "get_surface", "get_icon_state",
-    "SMALL_ICON_SIZE", "STANDARD_ICON_SIZE", "LARGE_ICON_SIZE"
+    "SMALL_ICON_SIZE", "STANDARD_ICON_SIZE", "LARGE_ICON_SIZE",
+    # "HTray", "VTray", "TrayButton", "TrayIcon",
+    # "ALIGN_TO_START", "ALIGN_TO_END", "GRID_CELL_SIZE",
+    # "Window", "UnfullscreenButton"
 ]
